fe5a341ee3b4652d8bc0b359b99749c5840365c6
Son Nguyen Xuan <Son.Nguyen.20022020@gmail.com>
2025-01-14 16:22:40 +0700

    Ememy waves - Timeline config

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,8 +1,13 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CollisionHandler : MonoBehaviour
 {
     [SerializeField] GameObject destroyVFX; 
+
+
+    // FUNFACT
+    /*Trong unity có một cơ chế, khi đó, nếu game có xử lý các sự kiện collider, ... 
+        thì class như đây sẽ ko xử lý hàm Start mà sẽ xử lý hàm trigger =)))*/
 
     private void OnTriggerEnter(Collider other)
     {
